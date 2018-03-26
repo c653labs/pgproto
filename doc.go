4657411a9c97beeeae8cc9d29e7ff502301560a7
brettlangdon <me@brett.is>
2018-03-26 17:04:42 -0400

    Add doc.go with start to package documentation

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+Package pgproto is a pure Go protocol library for PostgreSQL.
+It provides the necessary structures and functions to parse and encode PostgreSQL messages.
+
+The scope of pgproto is only for parsing/encoding messages and does not handle connections between
+PostgreSQL client and server.
+*/
+package pgproto
