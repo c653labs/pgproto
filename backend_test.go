45318a6c0c6cd0e51c16ed993063fc4335fdf966
brettlangdon <me@brett.is>
2018-06-11 21:54:07 -0400

    Add benchmark benchmark tests

--- backend_test.go
+++ backend_test.go
@@ -36,11 +36,38 @@ func (s *BackendKeyDataTestSuite) Test_ParseBackendKeyData_MD5() {
 	s.Equal(raw, backend.Encode())
 }
 
+func BenchmarkBackendKeyData_MD5(b *testing.B) {
+	raw := []byte{
+		// Tag
+		'K',
+		// Length
+		'\x00', '\x00', '\x00', '\x0c',
+		// PID
+		'\x00', '\x00', '\x04', '\xd2',
+		// Key
+		'\x00', '\x00', '\x04', '\xd2',
+	}
+
+	for i := 0; i < b.N; i++ {
+		_, err := pgproto.ParseBackendKeyData(bytes.NewReader(raw))
+		if err != nil {
+			b.Error(err)
+		}
+	}
+}
+
 func (s *BackendKeyDataTestSuite) Test_ParseBackendKeyData_Empty() {
 	backend, err := pgproto.ParseBackendKeyData(bytes.NewReader([]byte{}))
 	s.NotNil(err)
 	s.Nil(backend)
 }
+
+func BenchmarkBackendKeyData_Empty(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		pgproto.ParseBackendKeyData(bytes.NewReader([]byte{}))
+	}
+}
+
 func (s *BackendKeyDataTestSuite) Test_BackendKeyDataEncode() {
 	expected := []byte{
 		// Tag
@@ -58,4 +85,14 @@ func (s *BackendKeyDataTestSuite) Test_BackendKeyDataEncode() {
 		Key: 1234,
 	}
 	s.Equal(expected, b.Encode())
+}
+
+func BenchmarkBackendKeyDataEncode(b *testing.B) {
+	m := &pgproto.BackendKeyData{
+		PID: 1234,
+		Key: 1234,
+	}
+	for i := 0; i < b.N; i++ {
+		m.Encode()
+	}
 }
